4c3099e4e9586f363e662a6beab492631c6fc447
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-06-26 10:44:12 +0200

    Initial work on a script that generates presentations.html

---
+++ scripts/presentations.php
@@ -0,0 +1,211 @@
+#!/usr/bin/env php
+<?php
+$reader = new XMLReader;
+$reader->open(__DIR__ . '/../presentations.xml');
+
+$presentations = array();
+$videos        = array();
+
+while ($reader->read()) {
+    if ($reader->nodeType !== XMLReader::ELEMENT) {
+        continue;
+    }
+
+    switch ($reader->name) {
+        case 'presentation': {
+            $presentations[] = new Presentation(
+                $reader->getAttribute('presenter'),
+                $reader->getAttribute('title'),
+                $reader->getAttribute('link'),
+                $reader->getAttribute('image')
+            );
+        }
+        break;
+
+        case 'video': {
+            if ($reader->getAttribute('type') == 'vimeo') {
+                $class = 'VimeoVideo';
+            } else {
+                $class = 'YouTubeVideo';
+            }
+
+            $videos[] = new $class(
+                $reader->getAttribute('presenter'),
+                $reader->getAttribute('title'),
+                $reader->getAttribute('id')
+            );
+        }
+    }
+}
+
+if (count($presentations) % 3 != 0) {
+    die('Number of presentations not divisible by 3');
+}
+
+if (count($videos) % 3 != 0) {
+    die('Number of presentations not divisible by 3');
+}
+
+$buffer = file_get_contents(__DIR__ . '/templates/presentations_header.html');
+
+$rows = count($videos) / 3;
+for ($row = 1; $row <= $rows; $row++) {
+
+    $left   = array_shift($videos);
+    $middle = array_shift($videos);
+    $right  = array_shift($videos);
+
+    $buffer .= sprintf(
+        '   <div class="row"><div class="col-md-4"><h3>%s</h3></div><div class="col-md-4"><h3>%s</h3></div><div class="col-md-4"><h3>%s</h3></div></div>',
+        $left->getTitle(),
+        $middle->getTitle(),
+        $right->getTitle()
+    );
+
+    $buffer .= sprintf(
+        '   <div class="row"><div class="col-md-4"><h4>by %s</h4></div><div class="col-md-4"><h4>by %s</h4></div><div class="col-md-4"><h4>by %s</h4></div></div>',
+        $left->getPresenter(),
+        $middle->getPresenter(),
+        $right->getPresenter()
+    );
+
+    $buffer .= sprintf(
+        '   <div class="row"><div class="col-md-4">%s</div><div class="col-md-4">%s</div><div class="col-md-4">%s</div></div>',
+        $left->getIframe(),
+        $middle->getIframe(),
+        $right->getIframe()
+    );
+}
+
+$buffer .= file_get_contents(__DIR__ . '/templates/presentations_middle.html');
+
+$rows = count($presentations) / 3;
+for ($row = 1; $row <= $rows; $row++) {
+    $left   = array_shift($presentations);
+    $middle = array_shift($presentations);
+    $right  = array_shift($presentations);
+
+    $buffer .= sprintf(
+        '   <div class="row"><div class="col-md-4"><h3>%s</h3></div><div class="col-md-4"><h3>%s</h3></div><div class="col-md-4"><h3>%s</h3></div></div>',
+        $left->getTitle(),
+        $middle->getTitle(),
+        $right->getTitle()
+    );
+
+    $buffer .= sprintf(
+        '   <div class="row"><div class="col-md-4"><h4>by %s</h4></div><div class="col-md-4"><h4>by %s</h4></div><div class="col-md-4"><h4>by %s</h4></div></div>',
+        $left->getPresenter(),
+        $middle->getPresenter(),
+        $right->getPresenter()
+    );
+
+    $buffer .= sprintf(
+        '   <div class="row"><div class="col-md-4"><p><a href="%s"><img class="img-responsive img-rounded" alt="%s: %s" src="%s" /></a></p></div><div class="col-md-4"><p><a href="%s"><img class="img-responsive img-rounded" alt="%s: %s" src="%s" /></a></p></div><div class="col-md-4"><p><a href="%s"><img class="img-responsive img-rounded" alt="%s: %s" src="%s" /></a></p></div></div>',
+        $left->getLink(),
+        $left->getPresenter(),
+        $left->getTitle(),
+        $left->getImage(),
+        $middle->getLink(),
+        $middle->getPresenter(),
+        $middle->getTitle(),
+        $middle->getImage(),
+        $right->getLink(),
+        $right->getPresenter(),
+        $right->getTitle(),
+        $right->getImage()
+    );
+}
+
+$buffer .= file_get_contents(__DIR__ . '/templates/presentations_footer.html');
+
+file_put_contents(__DIR__ . '/../public/presentations.html', $buffer);
+
+class Presentation
+{
+    private $presenter;
+    private $title;
+    private $link;
+    private $image;
+
+    public function __construct($presenter, $title, $link, $image)
+    {
+        $this->presenter = $presenter;
+        $this->title     = $title;
+        $this->link      = $link;
+        $this->image     = $image;
+    }
+
+    public function getPresenter()
+    {
+        return $this->presenter;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+}
+
+abstract class Video
+{
+    private $presenter;
+    private $title;
+    private $id;
+
+    public function __construct($presenter, $title, $id)
+    {
+        $this->presenter = $presenter;
+        $this->title     = $title;
+        $this->id        = $id;
+    }
+
+    public function getPresenter()
+    {
+        return $this->presenter;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    abstract public function getIframe();
+}
+
+class VimeoVideo extends Video
+{
+    public function getIframe()
+    {
+        return sprintf(
+            '<div class="flex-video vimeo"><iframe src="http://player.vimeo.com/video/%s?title=0&amp;byline=0&amp;portrait=0"></iframe></div>',
+            $this->getId()
+        );
+    }
+}
+
+class YouTubeVideo extends Video
+{
+    public function getIframe()
+    {
+        return sprintf(
+            '<div class="flex-video widescreen"><iframe src="http://www.youtube-nocookie.com/embed/%s?rel=0&amp;wmode=opaque&amp;showinfo=0&amp;rel=0&amp;theme=light"></iframe></div>',
+            $this->getId()
+        );
+    }
+}
+
