0afeabb4d1ef1dda17cdf4a6526c93ee2a1ed464
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-02-10 10:02:02 +0100

    Initial work on a script that renders build-status.html

---
+++ bin/update-build-status.php
@@ -0,0 +1,103 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+$next    = '9.1';
+$current = '9.0';
+$old     = '8.5';
+
+$nextRepositories = [
+    'phpunit'                  => 'master',
+    'php-code-coverage'        => 'master',
+    'code-unit-reverse-lookup' => 'master',
+    'comparator'               => 'master',
+    'diff'                     => 'master',
+    'environment'              => 'master',
+    'exporter'                 => 'master',
+    'global-state'             => 'master',
+    'object-enumerator'        => 'master',
+    'object-reflector'         => 'master',
+    'php-invoker'              => 'master',
+    'php-timer'                => 'master',
+    'php-token-stream'         => 'master',
+    'recursion-context'        => 'master',
+    'type'                     => 'master',
+];
+
+$currentRepositories = [
+    'phpunit'                  => '9.0',
+    'php-code-coverage'        => 'master',
+    'code-unit-reverse-lookup' => 'master',
+    'comparator'               => 'master',
+    'diff'                     => 'master',
+    'environment'              => 'master',
+    'exporter'                 => 'master',
+    'global-state'             => 'master',
+    'object-enumerator'        => 'master',
+    'object-reflector'         => 'master',
+    'php-invoker'              => 'master',
+    'php-timer'                => 'master',
+    'php-token-stream'         => 'master',
+    'recursion-context'        => 'master',
+    'type'                     => 'master',
+];
+
+$oldRepositories = [
+    'phpunit'                  => '8.5',
+    'php-code-coverage'        => '7.0',
+    'code-unit-reverse-lookup' => '1.0',
+    'comparator'               => '3.0',
+    'diff'                     => '3.0',
+    'environment'              => '4.2',
+    'exporter'                 => '3.1',
+    'global-state'             => '3.0',
+    'object-enumerator'        => '3.0',
+    'object-reflector'         => '2.0',
+    'php-invoker'              => '2.0',
+    'php-timer'                => '2.1',
+    'php-token-stream'         => '3.1',
+    'recursion-context'        => '3.0',
+    'type'                     => '1.1',
+];
+
+\file_put_contents(
+    __DIR__ . '/../public/build-status.html',
+    \str_replace(
+        [
+            '{{next}}',
+            '{{next_items}}',
+            '{{current}}',
+            '{{current_items}}',
+            '{{old}}',
+            '{{old_items}}',
+        ],
+        [
+            $next,
+            render($nextRepositories),
+            $current,
+            render($currentRepositories),
+            $old,
+            render($oldRepositories),
+        ],
+        \file_get_contents(__DIR__ . '/../templates/build-status-page.html')
+    )
+);
+
+function render(array $repositories): string
+{
+    $buffer = '';
+
+    foreach ($repositories as $repository => $branch) {
+        $buffer .= \str_replace(
+            [
+                '{{repository}}',
+                '{{branch}}',
+            ],
+            [
+                $repository,
+                $branch,
+            ],
+            \file_get_contents(__DIR__ . '/../templates/build-status-item.html')
+        );
+    }
+
+    return $buffer;
+}
